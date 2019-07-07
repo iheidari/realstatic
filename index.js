8c7742cb7eb8fde309b97871c2d5a5df88630bb1
iheidari <iman.heydari@gmail.com>
2019-07-06 17:27:06 -0700

    add rew search

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const propAddress = "2062 Mary Hill Road";
+const propAddress = "2975 East Lake Gate";
 
 const baseUrl = "https://www.bcassessment.ca/";
-getInfo(propAddress);
+//getInfo(propAddress);
+
+// getPropInfoByRealtorUrl(
+//   "https://www.realtor.ca/real-estate/20851392/3-bedroom-condo-424-cardiff-way-port-moody"
+// );
+//https://www.rew.ca/properties/R2371752/2718-daybreak-avenue-coquitlam-bc
+
+getPropInfoByRewUrl(
+  "https://www.rew.ca/properties/R2371752/2718-daybreak-avenue-coquitlam-bc"
+);
 
 async function getInfo(address) {
   const addressInfo = await getByAddress(address);
@@ -27,6 +36,7 @@ async function getById(id) {
   try {
     const response = await axios.get(url);
     const toRet = parsePropertyInfo(response.data);
+    console.log(toRet);
     return toRet;
   } catch (error) {
     console.error(error);
@@ -77,4 +87,36 @@ function parsePropertyInfo(propertyInfoHtml) {
 
 function getValueBySelector($, selector) {
   return $(selector).text();
+}
+
+async function getPropInfoByRealtorUrl(realtorurl) {
+  try {
+    const response = await axios.get(realtorurl);
+    const toRet = parseRealtorInfo(response.data);
+    return toRet;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function parseRealtorInfo(html) {
+  const $ = cheerio.load(html);
+  const listingPrice = getValueBySelector($, "#listingPrice");
+  console.log(listingPrice);
+}
+
+async function getPropInfoByRewUrl(url) {
+  try {
+    const response = await axios.get(url);
+    const toRet = parseRewInfo(response.data);
+    return toRet;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function parseRewInfo(html) {
+  const $ = cheerio.load(html);
+  const listingPrice = getValueBySelector($, ".propertyheader-price");
+  console.log(listingPrice);
 }
